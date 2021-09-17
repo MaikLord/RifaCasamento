8ad078f5a23535d0858d571c522eb5d273c66b79
Lucas José <77784071+MaikLord@users.noreply.github.com>
2021-09-16 22:10:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,409 @@
+
+const num1 = document.getElementById('1').innerText;
+const num2 = document.getElementById('2').innerText;
+const num3 = document.getElementById('3').innerText;
+const num4 = document.getElementById('4').innerText;
+const num5 = document.getElementById('5').innerText;
+const num6 = document.getElementById('6').innerText;
+const num7 = document.getElementById('7').innerText;
+const num8 = document.getElementById('8').innerText;
+const num9 = document.getElementById('9').innerText;
+const num10 = document.getElementById('10').innerText;
+const num11 = document.getElementById('11').innerText;
+const num12 = document.getElementById('12').innerText;
+const num13 = document.getElementById('13').innerText;
+const num14 = document.getElementById('14').innerText;
+const num15 = document.getElementById('15').innerText;
+const num16 = document.getElementById('16').innerText;
+const num17 = document.getElementById('17').innerText;
+const num18 = document.getElementById('18').innerText;
+const num19 = document.getElementById('19').innerText;
+const num20 = document.getElementById('20').innerText;
+const num21 = document.getElementById('21').innerText;
+const num22 = document.getElementById('22').innerText;
+const num23 = document.getElementById('23').innerText;
+const num24 = document.getElementById('24').innerText;
+const num25 = document.getElementById('25').innerText;
+const num26 = document.getElementById('26').innerText;
+const num27 = document.getElementById('27').innerText;
+const num28 = document.getElementById('28').innerText;
+const num29 = document.getElementById('29').innerText;
+const num30 = document.getElementById('30').innerText;
+const num31 = document.getElementById('31').innerText;
+const num32 = document.getElementById('32').innerText;
+const num33 = document.getElementById('33').innerText;
+const num34 = document.getElementById('34').innerText;
+const num35 = document.getElementById('35').innerText;
+const num36 = document.getElementById('36').innerText;
+const num37 = document.getElementById('37').innerText;
+const num38 = document.getElementById('38').innerText;
+const num39 = document.getElementById('39').innerText;
+const num40 = document.getElementById('40').innerText;
+const num41 = document.getElementById('41').innerText;
+const num42 = document.getElementById('42').innerText;
+const num43 = document.getElementById('43').innerText;
+const num44 = document.getElementById('44').innerText;
+const num45 = document.getElementById('45').innerText;
+const num46 = document.getElementById('46').innerText;
+const num47 = document.getElementById('47').innerText;
+const num48 = document.getElementById('48').innerText;
+const num49 = document.getElementById('49').innerText;
+const num50 = document.getElementById('50').innerText;
+const num51 = document.getElementById('51').innerText;
+const num52 = document.getElementById('52').innerText;
+const num53 = document.getElementById('53').innerText;
+const num54 = document.getElementById('34').innerText;
+const num55 = document.getElementById('55').innerText;
+const num56 = document.getElementById('56').innerText;
+const num57 = document.getElementById('57').innerText;
+const num58 = document.getElementById('58').innerText;
+const num59 = document.getElementById('59').innerText;
+const num60 = document.getElementById('60').innerText;
+const num61 = document.getElementById('61').innerText;
+const num62 = document.getElementById('62').innerText;
+const num63 = document.getElementById('63').innerText;
+const num64 = document.getElementById('64').innerText;
+const num65 = document.getElementById('65').innerText;
+const num66 = document.getElementById('66').innerText;
+const num67 = document.getElementById('67').innerText;
+const num68 = document.getElementById('68').innerText;
+const num69 = document.getElementById('69').innerText;
+const num70 = document.getElementById('70').innerText;
+const num71 = document.getElementById('71').innerText;
+const num72 = document.getElementById('72').innerText;
+const num73 = document.getElementById('73').innerText;
+const num74 = document.getElementById('74').innerText;
+const num75 = document.getElementById('75').innerText;
+const num76 = document.getElementById('76').innerText;
+const num77 = document.getElementById('77').innerText;
+const num78 = document.getElementById('78').innerText;
+const num79 = document.getElementById('79').innerText;
+const num80 = document.getElementById('80').innerText;
+const num81 = document.getElementById('81').innerText;
+const num82 = document.getElementById('82').innerText;
+const num83 = document.getElementById('83').innerText;
+const num84 = document.getElementById('84').innerText;
+const num85 = document.getElementById('85').innerText;
+const num86 = document.getElementById('86').innerText;
+const num87 = document.getElementById('87').innerText;
+const num88 = document.getElementById('88').innerText;
+const num89 = document.getElementById('89').innerText;
+const num90 = document.getElementById('90').innerText;
+const num91 = document.getElementById('91').innerText;
+const num92 = document.getElementById('92').innerText;
+const num93 = document.getElementById('93').innerText;
+const num94 = document.getElementById('94').innerText;
+const num95 = document.getElementById('95').innerText;
+const num96 = document.getElementById('96').innerText;
+const num97 = document.getElementById('97').innerText;
+const num98 = document.getElementById('98').innerText;
+const num99 = document.getElementById('99').innerText;
+const num100 = document.getElementById('100').innerText;
+
+function gerarNum() {
+
+    const mathResult = Math.floor(Math.random() * (99 + 1)) + 1;
+    document.getElementById("numeroOK").innerText = mathResult;
+    document.getElementById("numeroOK").style.animation = "animate 2s alternate infinite";
+
+    if (mathResult == num1) {
+        document.getElementById('1').style.background = "#202020";
+    }
+    if (mathResult == num2) {
+        document.getElementById('2').style.background = "#202020";
+    }
+    if (mathResult == num3) {
+        document.getElementById('3').style.background = "#202020";
+    }
+    if (mathResult == num4) {
+        document.getElementById('4').style.background = "#202020";
+    }
+    if (mathResult == num5) {
+        document.getElementById('5').style.background = "#202020";
+    }
+    if (mathResult == num6) {
+        document.getElementById('6').style.background = "#202020";
+    }
+    if (mathResult == num7) {
+        document.getElementById('7').style.background = "#202020";
+    }
+    if (mathResult == num8) {
+        document.getElementById('8').style.background = "#202020";
+    }
+    if (mathResult == num9) {
+        document.getElementById('9').style.background = "#202020";
+    }
+    if (mathResult == num10) {
+        document.getElementById('10').style.background = "#202020";
+    }
+    if (mathResult == num11) {
+        document.getElementById('11').style.background = "#202020";
+    }
+    if (mathResult == num12) {
+        document.getElementById('12').style.background = "#202020";
+    }
+    if (mathResult == num13) {
+        document.getElementById('13').style.background = "#202020";
+    }
+    if (mathResult == num14) {
+        document.getElementById('14').style.background = "#202020";
+    }
+    if (mathResult == num15) {
+        document.getElementById('15').style.background = "#202020";
+    }
+    if (mathResult == num16) {
+        document.getElementById('16').style.background = "#202020";
+    }
+    if (mathResult == num17) {
+        document.getElementById('17').style.background = "#202020";
+    }
+    if (mathResult == num18) {
+        document.getElementById('18').style.background = "#202020";
+    }
+    if (mathResult == num19) {
+        document.getElementById('19').style.background = "#202020";
+    }
+    if (mathResult == num20) {
+        document.getElementById('20').style.background = "#202020";
+    }
+    if (mathResult == num21) {
+        document.getElementById('21').style.background = "#202020";
+    }
+    if (mathResult == num22) {
+        document.getElementById('22').style.background = "#202020";
+    }
+    if (mathResult == num23) {
+        document.getElementById('23').style.background = "#202020";
+    }
+    if (mathResult == num24) {
+        document.getElementById('24').style.background = "#202020";
+    }
+    if (mathResult == num25) {
+        document.getElementById('25').style.background = "#202020";
+    }
+    if (mathResult == num26) {
+        document.getElementById('26').style.background = "#202020";
+    }
+    if (mathResult == num27) {
+        document.getElementById('27').style.background = "#202020";
+    }
+    if (mathResult == num28) {
+        document.getElementById('28').style.background = "#202020";
+    }
+    if (mathResult == num29) {
+        document.getElementById('29').style.background = "#202020";
+    }
+    if (mathResult == num30) {
+        document.getElementById('30').style.background = "#202020";
+    }
+    if (mathResult == num31) {
+        document.getElementById('31').style.background = "#202020";
+    }
+    if (mathResult == num32) {
+        document.getElementById('32').style.background = "#202020";
+    }
+    if (mathResult == num33) {
+        document.getElementById('33').style.background = "#202020";
+    }
+    if (mathResult == num34) {
+        document.getElementById('34').style.background = "#202020";
+    }
+    if (mathResult == num35) {
+        document.getElementById('35').style.background = "#202020";
+    }
+    if (mathResult == num36) {
+        document.getElementById('36').style.background = "#202020";
+    }
+    if (mathResult == num37) {
+        document.getElementById('37').style.background = "#202020";
+    }
+    if (mathResult == num38) {
+        document.getElementById('38').style.background = "#202020";
+    }
+    if (mathResult == num39) {
+        document.getElementById('39').style.background = "#202020";
+    }
+    if (mathResult == num40) {
+        document.getElementById('40').style.background = "#202020";
+    }
+    if (mathResult == num41) {
+        document.getElementById('41').style.background = "#202020";
+    }
+    if (mathResult == num42) {
+        document.getElementById('42').style.background = "#202020";
+    }
+    if (mathResult == num43) {
+        document.getElementById('43').style.background = "#202020";
+    }
+    if (mathResult == num44) {
+        document.getElementById('44').style.background = "#202020";
+    }
+    if (mathResult == num45) {
+        document.getElementById('45').style.background = "#202020";
+    }
+    if (mathResult == num46) {
+        document.getElementById('46').style.background = "#202020";
+    }
+    if (mathResult == num47) {
+        document.getElementById('47').style.background = "#202020";
+    }
+    if (mathResult == num48) {
+        document.getElementById('48').style.background = "#202020";
+    }
+    if (mathResult == num49) {
+        document.getElementById('49').style.background = "#202020";
+    }
+    if (mathResult == num50) {
+        document.getElementById('50').style.background = "#202020";
+    }
+    if (mathResult == num51) {
+        document.getElementById('51').style.background = "#202020";
+    }
+    if (mathResult == num52) {
+        document.getElementById('52').style.background = "#202020";
+    }
+    if (mathResult == num53) {
+        document.getElementById('53').style.background = "#202020";
+    }
+    if (mathResult == num54) {
+        document.getElementById('54').style.background = "#202020";
+    }
+    if (mathResult == num55) {
+        document.getElementById('55').style.background = "#202020";
+    }
+    if (mathResult == num56) {
+        document.getElementById('56').style.background = "#202020";
+    }
+    if (mathResult == num57) {
+        document.getElementById('57').style.background = "#202020";
+    }
+    if (mathResult == num58) {
+        document.getElementById('58').style.background = "#202020";
+    }
+    if (mathResult == num59) {
+        document.getElementById('59').style.background = "#202020";
+    }
+    if (mathResult == num60) {
+        document.getElementById('60').style.background = "#202020";
+    }
+    if (mathResult == num61) {
+        document.getElementById('61').style.background = "#202020";
+    }
+    if (mathResult == num62) {
+        document.getElementById('62').style.background = "#202020";
+    }
+    if (mathResult == num63) {
+        document.getElementById('63').style.background = "#202020";
+    }
+    if (mathResult == num64) {
+        document.getElementById('64').style.background = "#202020";
+    }
+    if (mathResult == num65) {
+        document.getElementById('65').style.background = "#202020";
+    }
+    if (mathResult == num66) {
+        document.getElementById('66').style.background = "#202020";
+    }
+    if (mathResult == num67) {
+        document.getElementById('67').style.background = "#202020";
+    }
+    if (mathResult == num68) {
+        document.getElementById('68').style.background = "#202020";
+    }
+    if (mathResult == num69) {
+        document.getElementById('69').style.background = "#202020";
+    }
+    if (mathResult == num70) {
+        document.getElementById('70').style.background = "#202020";
+    }
+    if (mathResult == num71) {
+        document.getElementById('71').style.background = "#202020";
+    }
+    if (mathResult == num72) {
+        document.getElementById('72').style.background = "#202020";
+    }
+    if (mathResult == num73) {
+        document.getElementById('73').style.background = "#202020";
+    }
+    if (mathResult == num74) {
+        document.getElementById('74').style.background = "#202020";
+    }
+    if (mathResult == num75) {
+        document.getElementById('75').style.background = "#202020";
+    }
+    if (mathResult == num76) {
+        document.getElementById('76').style.background = "#202020";
+    }
+    if (mathResult == num77) {
+        document.getElementById('77').style.background = "#202020";
+    }
+    if (mathResult == num78) {
+        document.getElementById('78').style.background = "#202020";
+    }
+    if (mathResult == num79) {
+        document.getElementById('79').style.background = "#202020";
+    }
+    if (mathResult == num80) {
+        document.getElementById('80').style.background = "#202020";
+    }
+    if (mathResult == num81) {
+        document.getElementById('81').style.background = "#202020";
+    }
+    if (mathResult == num82) {
+        document.getElementById('82').style.background = "#202020";
+    }
+    if (mathResult == num83) {
+        document.getElementById('83').style.background = "#202020";
+    }
+    if (mathResult == num84) {
+        document.getElementById('84').style.background = "#202020";
+    }
+    if (mathResult == num85) {
+        document.getElementById('85').style.background = "#202020";
+    }
+    if (mathResult == num86) {
+        document.getElementById('86').style.background = "#202020";
+    }
+    if (mathResult == num87) {
+        document.getElementById('87').style.background = "#202020";
+    }
+    if (mathResult == num88) {
+        document.getElementById('88').style.background = "#202020";
+    }
+    if (mathResult == num89) {
+        document.getElementById('89').style.background = "#202020";
+    }
+    if (mathResult == num90) {
+        document.getElementById('90').style.background = "#202020";
+    }
+    if (mathResult == num91) {
+        document.getElementById('91').style.background = "#202020";
+    }
+    if (mathResult == num92) {
+        document.getElementById('92').style.background = "#202020";
+    }
+    if (mathResult == num93) {
+        document.getElementById('93').style.background = "#202020";
+    }
+    if (mathResult == num94) {
+        document.getElementById('94').style.background = "#202020";
+    }
+    if (mathResult == num95) {
+        document.getElementById('95').style.background = "#202020";
+    }
+    if (mathResult == num96) {
+        document.getElementById('96').style.background = "#202020";
+    }
+    if (mathResult == num97) {
+        document.getElementById('97').style.background = "#202020";
+    }
+    if (mathResult == num98) {
+        document.getElementById('98').style.background = "#202020";
+    }
+    if (mathResult == num99) {
+        document.getElementById('99').style.background = "#202020";
+    }
+    if (mathResult == num100) {
+        document.getElementById('100').style.background = "#202020";
+    }
+};
